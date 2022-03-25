c4a755db95e715af25f8494ad17ed3c2389aacc3
talilotarlison <101112484+talilotarlison@users.noreply.github.com>
2022-03-25 01:02:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var myMusicInputID = myMusicInput.substr(17)
 var linkMusicNaTela = document.getElementById("video");
 var linkEmbedID = "https://www.youtube.com/embed/" + myMusicInputID
 var linkIframe = '<iframe width="670" height="640" src="' +linkEmbedID+ '" title="player" frameborder="0" allow="autoplay; clipboard-write;"></iframe>'
-var linkArtista = '<h1><font color="white">Faixa Desconhecida</font><h1><h1><font color="gray">Artista Desconhecido</font></h1>'
+var linkArtista = '<h1><font color="white">Faixa Desconhecida</font><h1><h2><font color="gray">Artista Desconhecido</font></h2>'
 var linkFinal = linkIframe + linkArtista
 
 console.log(linkFinal)
